ca64b1f05e88fdcc5b6f17c8955bad967d6e885b
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-03-17 18:35:09 -0300

    Navbar reestilizado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Home.css';
-import bannerImage from '../assets/images/banner.jpg';
 
 function Home() {
   return (
     <div className="home">
-      <div className="home-banner" style={{ backgroundImage: `url(${bannerImage})` }}></div>
       <div className="home-content">
-        <header className="home-header">
+        <header className="header">
           <h1>𝗭𝗘𝗥𝗢𝟮𝟬 𝗚𝗔𝗥𝗔𝗚𝗘™</h1>
           <p>Oficina Mecânica e Retífica de Motores Nacionais e Importados</p>
         </header>
