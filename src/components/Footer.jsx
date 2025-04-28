27a0fb7cbc6c4329f9c249fe3228038dc1d56a64
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-04-28 17:36:15 -0300

    Reestruturação do footer,  monu mobile com arcodeon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,104 +1,146 @@
-import React from 'react';
-import '../styles/Footer.css' // Importa o arquivo global de estilos
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
-import { MdPix } from 'react-icons/md'; // Ícone do Pix
-import { FaCcVisa, FaCcMastercard, FaBarcode } from 'react-icons/fa'; // Ícones de pagamento
+import React, { useState } from 'react';
+import '../styles/Footer.css';
+import { FaFacebook, FaInstagram, FaCcVisa, FaCcMastercard, FaBarcode } from 'react-icons/fa';
+import { MdPix } from 'react-icons/md';
 
 function Footer() {
   const formasPagamento = [
     { nome: 'Visa', icone: <FaCcVisa /> },
     { nome: 'Mastercard', icone: <FaCcMastercard /> },
     { nome: 'Boleto', icone: <FaBarcode /> },
-    { nome: 'Pix', icone: <MdPix /> }, // Adicionado o Pix
+    { nome: 'Pix', icone: <MdPix /> },
   ];
 
-  const facebook = [
-    { nome: '', link: 'https://www.facebook.com/zero20garage', icone: <FaFacebook className="icon-medium" /> },
-  ];
+  const [activeMenu, setActiveMenu] = useState(null);
 
-  const instagram = [
-    { nome: '', link: 'https://www.instagram.com/zero20garage', icone: <FaInstagram /> },
-  ];
+  const toggleMenu = (menu) => {
+    setActiveMenu(activeMenu === menu ? null : menu);
+  };
 
   return (
     <footer className="footer">
       <div className="footer-content">
-      <div className="footer-content-wrapper">
-        
-      {/* Seção de navegação */}
-      <div className="footer-nav">
-        <ul>
-          <li><a href="/politica">Política de Privacidade</a></li>
-          <li><a href="/termos">Termos de Uso</a></li>
-          <li><a href="/trocas">Trocas e Devoluções</a></li>
-          <li><a href="/faq">FAQ</a></li>
-        </ul>
-      </div>
 
-      <div className="footer-nav">
-        <ul>
-          <li><a href="/servicos">Serviços</a></li>
-          <li><a href="/sobre">Sobre Nós</a></li>
-          <li><a href="/contato">Contato</a></li>
-          <li><a href="/blog" translate="no">Blog</a></li>
-          <li><a href="/trabalhe-conosco">Trabalhe Conosco</a></li>
-        </ul>
-      </div>
-      </div>
+        {/* DESKTOP FOOTER */}
+        <div className="footer-desktop">
+          <div className="footer-content-wrapper">
+            {/* Seções de Navegação */}
+            <div className="footer-nav">
+              <ul>
+                <li><a href="/politica">Política de Privacidade</a></li>
+                <li><a href="/termos">Termos de Uso</a></li>
+                <li><a href="/trocas">Trocas e Devoluções</a></li>
+                <li><a href="/faq">FAQ</a></li>
+              </ul>
+            </div>
 
-      <div className="footer-content-wrapper-Payment">
-        {/* Seção Formas de Pagamento */}
-        <div className="footer-card">
-          <span className="title">Formas de Pagamento:</span>
-          <div className="icons">
-            {formasPagamento.map((formaPagamento) => (
-              <span 
-                key={formaPagamento.nome}
-                className="payment-icon"
-                title={formaPagamento.nome} // Adicionado o tooltip
-              >
-                {formaPagamento.icone}
-              </span>
-            ))}
+            <div className="footer-nav">
+              <ul>
+                <li><a href="/servicos">Serviços</a></li>
+                <li><a href="/sobre">Sobre Nós</a></li>
+                <li><a href="/contato">Contato</a></li>
+                <li><a href="/blog" translate="no">Blog</a></li>
+                <li><a href="/trabalhe-conosco">Trabalhe Conosco</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="footer-content-wrapper-Payment">
+            <div className="footer-card">
+              <span className="title">Formas de Pagamento:</span>
+              <div className="icons">
+                {formasPagamento.map((forma) => (
+                  <span key={forma.nome} className="payment-icon" title={forma.nome}>
+                    {forma.icone}
+                  </span>
+                ))}
+              </div>
+            </div>
+
+            <div className="footer-card">
+              <span className="title">Siga-nos:</span>
+              <div className="icons">
+                <a href="https://www.facebook.com/zero20garage" target="_blank" rel="noopener noreferrer">
+                  <FaFacebook className="icon-medium" />
+                </a>
+                <a href="https://www.instagram.com/zero20garage" target="_blank" rel="noopener noreferrer">
+                  <FaInstagram className="icon-medium" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="footer-card">
+            <span className="title">© 2025 𝗭𝗘𝗥𝗢 𝟮𝟬 𝗚𝗔𝗥𝗔𝗚𝗘™ Todos os direitos reservados.</span>
           </div>
         </div>
 
-        {/* Seção Siga-nos */}
-        <div className="footer-card">
-          <span className="title">Siga-nos:</span>
-          <div className="icons">
-            {facebook.map((facebook) => (
-              <a
-                key={facebook.nome}
-                href={facebook.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ margin: "1rem" }} // Adiciona o espaçamento entre os ícones
-              >
-                {facebook.nome}
-                <i className="fa fa-facebook"></i>
-              </a>
-            ))}
+        {/* MOBILE FOOTER */}
+        <div className="footer-mobile">
+          <div className="accordion">
+            <div className="accordion-item">
+              <button onClick={() => toggleMenu('sobre')} className="accordion-button">Sobre a Zero 20 Garagem</button>
+              {activeMenu === 'sobre' && (
+                <ul className="accordion-content">
+                  <li><a href="/sobre">Sobre Nós</a></li>
+                  <li><a href="/trabalhe-conosco">Trabalhe Conosco</a></li>
+                </ul>
+              )}
+            </div>
 
-            {instagram.map((instagram) => (
-              <a
-                key={instagram.nome}
-                href={instagram.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ margin: "1rem" }} // Adiciona o espaçamento entre os ícones
-              >
-                {instagram.nome}
-                <i className="fa fa-instagram"></i>
-              </a>
-            ))}
+            <div className="accordion-item">
+              <button onClick={() => toggleMenu('privacidade')} className="accordion-button">Portal de Privacidade</button>
+              {activeMenu === 'privacidade' && (
+                <ul className="accordion-content">
+                  <li><a href="/politica">Política de Privacidade</a></li>
+                  <li><a href="/faq">FAQ</a></li>
+                </ul>
+              )}
+            </div>
+
+            <div className="accordion-item">
+              <button onClick={() => toggleMenu('atendimento')} className="accordion-button">Atendimento</button>
+              {activeMenu === 'atendimento' && (
+                <ul className="accordion-content">
+                  <li><a href="/trocas">Trocas e Devoluções</a></li>
+                  <li><a href="/termos">Termos de Uso</a></li>
+                </ul>
+              )}
+            </div>
+
+            <div className="accordion-item">
+              <button onClick={() => toggleMenu('redes')} className="accordion-button">Zero 20 nas Redes</button>
+              {activeMenu === 'redes' && (
+                <div className="icons accordion-content">
+                  <a href="https://www.facebook.com/zero20garage" target="_blank" rel="noopener noreferrer">
+                    <FaFacebook className="icon-medium" />
+                  </a>
+                  <a href="https://www.instagram.com/zero20garage" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram className="icon-medium" />
+                  </a>
+                </div>
+              )}
+            </div>
+
+            <div className="accordion-item">
+              <button onClick={() => toggleMenu('pagamento')} className="accordion-button">Formas de Pagamento</button>
+              {activeMenu === 'pagamento' && (
+                <div className="icons accordion-content">
+                  {formasPagamento.map((forma) => (
+                    <span key={forma.nome} className="payment-icon" title={forma.nome}>
+                      {forma.icone}
+                    </span>
+                  ))}
+                </div>
+              )}
+            </div>
+
+            <div className="footer-card">
+              <span className="title">© 2025 𝗭𝗘𝗥𝗢 𝟮𝟬 𝗚𝗔𝗥𝗔𝗚𝗘™</span>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="footer-card">
-        <span className="title">© 2025 𝗭𝗘𝗥𝗢 𝟮𝟬 𝗚𝗔𝗥𝗔𝗚𝗘™ Todos os direitos reservados.</span>
-        </div>
       </div>
     </footer>
   );
