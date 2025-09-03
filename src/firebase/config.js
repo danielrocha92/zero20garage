a85a76ca16c69ade6a09282b73cf83fa21c40350
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-09-02 23:01:36 -0300

    ok

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,10 @@
-// src/firebase/config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyAbCdEfGhIjKlMnOpQrStUvWxYz123456",
-  authDomain: "zer20garage.firebaseapp.com",
-  projectId: "zer20garage",
-  storageBucket: "zer20garage.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890"
+  apiKey: "AIzaSyAbCdEfGhIjKlMnOpQrStUvWxYz123456",  // Chave da API, usada para identificar o aplicativo em seu projeto Firebase.
+  authDomain: "zer20garage.firebaseapp.com",  // O domínio do Firebase Authentication, usado para autenticação.
+  projectId: "zer20garage",  // O ID do seu projeto no Firebase.
+  storageBucket: "zer20garage.appspot.com",  // O bucket de armazenamento do Firebase Storage, onde você pode armazenar arquivos.
+  messagingSenderId: "123456789012",  // O ID do remetente para mensagens, usado em Firebase Cloud Messaging (FCM).
+  appId: "1:123456789012:web:abcdef1234567890"  // O ID do aplicativo, necessário para identificar seu app no Firebase.
 };
 
 export default firebaseConfig;
