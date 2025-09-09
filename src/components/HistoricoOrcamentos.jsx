88cdede2141972a871e6fa9f16cca93539f7c5e2
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-09-09 19:15:46 -0300

    Atualizar o HistoricoOrcamentos.jsx

--- src/components/HistoricoOrcamentos.jsx
+++ src/components/HistoricoOrcamentos.jsx
@@ -1,8 +1,14 @@
 // src/components/HistoricoOrcamentos.jsx
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// src/components/HistoricoOrcamentos.jsx
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios'; // Remova esta linha
 import './HistoricoOrcamentos.css';
 import { FaEye, FaEdit, FaTrash } from 'react-icons/fa';
+
+// Importe a instância do banco de dados (db) e as funções do Firestore
+import { db } from '../firebase';
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore'; 
 
 const API_BASE_URL = 'https://api-orcamento-n49u.onrender.com';
 
