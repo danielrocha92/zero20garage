0772de4e01cfcd2e82dd6fb3cf45dca9ac79d6b1
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-09-09 19:06:47 -0300

    Atualizar o config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,20 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // Importe o Firestore aqui
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAbCdEfGhIjKlMnOpQrStUvWxYz123456",  // Chave da API, usada para identificar o aplicativo em seu projeto Firebase.
-  authDomain: "zer20garage.firebaseapp.com",  // O domínio do Firebase Authentication, usado para autenticação.
-  projectId: "zer20garage",  // O ID do seu projeto no Firebase.
-  storageBucket: "zer20garage.appspot.com",  // O bucket de armazenamento do Firebase Storage, onde você pode armazenar arquivos.
-  messagingSenderId: "123456789012",  // O ID do remetente para mensagens, usado em Firebase Cloud Messaging (FCM).
-  appId: "1:123456789012:web:abcdef1234567890"  // O ID do aplicativo, necessário para identificar seu app no Firebase.
+  apiKey: "AIzaSyChLSQrE2uma5PHXEmbzgxRLE6WbwCw0CM",
+  authDomain: "zero20-upload-api-e3a14.firebaseapp.com",
+  projectId: "zero20-upload-api-e3a14",
+  storageBucket: "zero20-upload-api-e3a14.firebasestorage.app",
+  messagingSenderId: "571940430324",
+  appId: "1:571940430324:web:d3a8579a3e31d9967dd9a1",
+  measurementId: "G-BQGXRLEPCJ"
 };
 
-export default firebaseConfig;
+// Inicialize o Firebase e o Firestore
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Exporte a instância do Firestore para usar em outros lugares
+export { db };
