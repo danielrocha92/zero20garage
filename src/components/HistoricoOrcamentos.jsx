2cdd34eb0a81bb5d1d99bf4c3aafdcbb5eb32efd
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-09-09 19:56:25 -0300

    Atualizar o HistoricoOrcamentos.jsx

--- src/components/HistoricoOrcamentos.jsx
+++ src/components/HistoricoOrcamentos.jsx
@@ -5,7 +5,7 @@ import './HistoricoOrcamentos.css';
 import { FaEye, FaEdit, FaTrash } from 'react-icons/fa';
 
 // Importe a instância do banco de dados (db) do seu arquivo de configuração local
-import { db } from '../firebase';
+import { db } from '../firebase/config';
 // Importe as funções do Firestore diretamente do pacote 'firebase/firestore'
 import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore'; 
 
