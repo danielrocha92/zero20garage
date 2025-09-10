46b6494b18043f258ced611a27183765cb89525b
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-09-10 10:45:16 -0300

    ok

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,12 @@
 // src/services/firebase.js
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import firebaseConfig from '../firebase/config';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "../firebase/config";
 
+// Evita inicializações duplicadas
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+// Cria a instância do Firestore vinculada ao app
+export const db = getFirestore(app);
 
 export default app;
