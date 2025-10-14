da30f9bdb2d597371f5dcf34307a264dc774dea6
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2025-10-14 19:56:50 -0300

    Atualizar o ModalConfirmacao.jsx

--- src/components/ModalConfirmacao.jsx
+++ src/components/ModalConfirmacao.jsx
@@ -1,24 +1,23 @@
-// src/components/ModalConfirmacao.jsx
 import React from "react";
 import "./ModalConfirmacao.css";
 
-const ModalConfirmacao = ({ 
-  titulo = "Confirmação", 
-  mensagem, 
-  onConfirmar, 
-  onCancelar 
+const ModalConfirmacao = ({
+  titulo = "Confirmação",
+  mensagem,
+  onConfirmar,
+  onCancelar,
 }) => {
   return (
-    <div className="modal-overlay">
-      <div className="modal-container">
+    <div className="confirmacao-modal-overlay">
+      <div className="confirmacao-modal-container">
         <h3>{titulo}</h3>
         <p>{mensagem}</p>
 
-        <div className="modal-actions">
-          <button className="btn-cancelar" onClick={onCancelar}>
+        <div className="confirmacao-modal-actions">
+          <button className="confirmacao-btn-cancelar" onClick={onCancelar}>
             Cancelar
           </button>
-          <button className="btn-confirmar" onClick={onConfirmar}>
+          <button className="confirmacao-btn-confirmar" onClick={onConfirmar}>
             Confirmar
           </button>
         </div>
