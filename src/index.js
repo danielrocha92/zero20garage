a185810c3262395f671c0d97328b68573ed8834e
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2026-03-17 23:30:25 -0300

    feat: Adiciona HelmetProvider para gerenciamento de meta tags e remove a dependência mixpanel-browser.

--- src/index.js
+++ src/index.js
@@ -6,13 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import './GlobalStyles.css';
 
 import { HelmetProvider } from 'react-helmet-async'; // ✅ Importa o provider
-import mixpanel from "mixpanel-browser";
 
-// Create an instance of the Mixpanel object, your token is already added to this snippet
-mixpanel.init('d34ec5ff7da40a3d35c3656cc422b8f9', {
-  autocapture: true,
-  record_sessions_percent: 100,
-});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
